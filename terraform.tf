7683c447376f80baf58a62d2e2aab9fc38a35cec
thakurrishabh1599 <rishabh1599@gmail.com>
2024-07-08 00:01:02 +0530

    terraform-eks

---
+++ terraform.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.46.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.16.1"
+    }
+  }
+backend "s3" {
+    bucket         	   = "terraform-eks23"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "eu-north-1"
+  }
+
+  required_version = "~> 1.3"
+}
